c38dc945a94dc82291c7ea0485885530b7308f54
j2thex <jnamed@gmail.com>
2024-10-30 15:57:21 +0100

    fixed linting in vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-    <script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+    <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
     </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
