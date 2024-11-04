31b306c131d4ab9c06e53b823568f467afe8daa3
j2thex <jnamed@gmail.com>
2024-11-04 16:21:47 +0100

    should work v02

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import StyledComponentsRegistry from './registry';
 
 export const metadata: Metadata = {
   title: 'Happy Farmer',
@@ -12,7 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    styledComponents: true
+  },
+  reactStrictMode: true
+};
 
 module.exports = nextConfig; 
