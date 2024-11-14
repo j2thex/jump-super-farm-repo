9f82cd61e50d003ce0cc0df34d511485d9fe7107
j2thex <jnamed@gmail.com>
2024-11-14 17:34:46 +0100

    telegram webapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      </head>
       <body style={{ 
         margin: 0, 
         padding: 0,
